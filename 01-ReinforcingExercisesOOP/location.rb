db45a8c63ae3a1a04f78919a0c9c306cdd702a12
Martin <martin.st8n@gmail.com>
2019-02-28 20:53:15 -0500

    created Location and Trip class. make one interact with eachother

---
+++ 01-ReinforcingExercisesOOP/location.rb
@@ -0,0 +1,50 @@
+class Location
+
+  # Instance methods
+  def initialize(name)
+    @name = name
+  end
+
+  #READERS GETTERS
+  def name
+    @name
+  end
+
+  #WRITTERS SETTERS
+  def name=(new_name)
+    @name = new_name
+  end
+
+end #end of Location class
+
+class Trip
+
+  def initialize
+    @destinations = []
+  end
+
+  # getters
+  def destinations
+    @destinations
+  end
+
+  def add_destination(destination)
+    @destinations << destination
+  end
+
+  def start_trip
+
+    puts "Began trip"
+    destinations.each_with_index do |destination, index|
+      next_location = destinations[ index + 1]
+
+      next unless next_location
+      puts "Travelled from #{destination.name} to #{next_location.name}"
+
+    end
+
+    puts "Ended trip"
+
+  end
+
+end #end of Trip class
