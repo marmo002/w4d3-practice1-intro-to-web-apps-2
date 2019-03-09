0dc1c63e83958f52b7c7c937622d75c6c0c46f03
Martin <martin.st8n@gmail.com>
2019-03-08 20:30:02 -0500

    Finished the assignment checking if contact exists in the crm file. If it does, render contacts/:id, else show sinatras 404 page

--- 02-CRMwebPart2-htmlCSS/crm.rb
+++ 02-CRMwebPart2-htmlCSS/crm.rb
@@ -18,7 +18,13 @@ end
 
 get '/contacts/:id' do
   @contact = Contact.find_by({id: params[:id].to_i})
-  erb :show_contact
+
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+  
 end
 
 after do
